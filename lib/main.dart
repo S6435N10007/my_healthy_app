bca68c0ddcad23ca0f609b146fbdaa35d93b8725
S6435N10007 <S6435N10007@live.sau.ac.th>
2023-03-27 22:26:40 +0700

    V1 by Piyawat : Createproject and add package

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:my_healthy_app/views/healthy_splashscreen_ui.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HealthySplashScreen(),
+    ),
+  );
+}
---
+++ lib/views/healthy_splashscreen_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HealthySplashScreen extends StatefulWidget {
+  const HealthySplashScreen({super.key});
+
+  @override
+  State<HealthySplashScreen> createState() => _HealthySplashScreenState();
+}
+
+class _HealthySplashScreenState extends State<HealthySplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
